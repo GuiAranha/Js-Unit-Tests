1d323733bbc580dae8fdceab79388de94e941455
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-17 15:42:23 -0300

    Requisito 1 - ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let averageScore = 0;
 
+  if (array.length === 0) {
+    return undefined;
+  }
+  
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    averageScore += array[i];
+  }
+  return Math.round(averageScore / array.length);
+};
 module.exports = average;
